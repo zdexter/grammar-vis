bf329c792aa64a037176e71ba80544f67acd1270
Zach Dexter <zach@databracket.com>
2012-10-25 17:42:29 -0400

    Change comment location. Grab nonterminals and terminals out of productions.

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,4 +1,31 @@
+function getCurrentSymbol(input) {
+}
+
+function getNesting() {
+}
+
+function getPossibleNextSymbols() {
+}
+
+/* Return:
+    The current symbol.
+    If inside a production, the nesting of the current symbol.
+    A typeahead buffer with all possible next symbol types. */
 function interpretCode(code) {
   currentSymbol = getCurrentSymbol(input);
-  possibleNextSymbols = getPossibleNextSymbols(input);
+  nesting = getNesting(input);
+  possibleNextSymbols = getPossibleNextSymbols(nesting, symbol); // Do I need both? Not sure
+
+  render(currentSymbol, nesting, possibleNextSymbols);
+}
+
+/* Display:
+  The current symbol, highlighted.
+  Nesting, if applicable.
+  All possible next symbols, if any.
+*/
+function render(currentSymbol, nesting, possibleNextSymbols) {
+  var currentSymbol = document.getElementById("#currentSymbol");
+  var nestRoot = document.getElementById("#nest");
+  var nextSymbolList = document.getElementById("#nextSymbolList");
 }
--- js/productions.js
+++ js/productions.js
@@ -13,11 +13,11 @@ SymbolEnum = {
 terminals = {}
 nonterminals = {}
 
+
+/* Return a map of the productions in grammarString.
+  Productions are delimited by lines, and identified by key in
+    strings like "key: value". */
 function getProductions(grammarString) {
-  /* Return a map of the productions in grammarString.
-    Productions are delimited by lines, and identified by key in
-      strings like "key: value".
-  */
   var linesPattern = /[^\r\n]+/g; // Global match on non-CR or non-newline
   var lines = grammarString.match(linesPattern);
   
@@ -29,32 +29,75 @@ function getProductions(grammarString) {
       throw("All productions must consist of a name" +
          SymbolEnum.DEFINITION + "production pair.");
     }
+    var key = production[0].replace(SymbolEnum.TERMINATION,"").trim();
+    var value = production[1].replace(SymbolEnum.TERMINATION,"").trim();
 
-    productions[production[0]
-      .replace(SymbolEnum.TERMINATION,"").trim()] = production[1]
-      .replace(SymbolEnum.TERMINATION,"").trim();
+    productions[key] = value;
   }
+  console.log(grammarString);
   return productions;
 }
 
+function is_defined(obj) {
+  return typeof(obj) != "undefined";
+}
+
+/* Add obj to hash if obj isn't already a key in hash. */
+function addIfNotPresent(hash, obj) {
+  if (!is_defined(hash[obj])) {
+    hash[obj] = true;
+  }
+}
+
+function isTerminal(symbol) {
+  for (char in SymbolEnum.TERMINAL_STRING) {
+    if (SymbolEnum.TERMINAL_STRING[char] == symbol[0]) {
+      return true;
+    }
+  } 
+  return false;
+}
+
+/* Return a hash of hashes of valid symbols in productions. */
 function getValidSymbols(productions) {
-  /* Return a map of valid symbols in productions.
-  */
-  var SYMBOL_DELIMITER = ",";
+  var symbolObj = {};
+  symbolObj.terminals = {};
+  symbolObj.nonterminals = {};
+
+  for (production in productions) {
+    var symbolsInProduction = productions[production].split(' ');
+    for (symbol in symbolsInProduction) {
+      symbol = symbolsInProduction[symbol];
+      if (isTerminal(symbol)) {
+        addIfNotPresent(symbolObj.terminals, symbol);
+      } else {
+        addIfNotPresent(symbolObj.nonterminals, symbol);
+      }
+    }
+  }
+  return symbolObj;
 }
 
 function buildGrammar(grammarString) {
   // Productions -> statements -> symbols -> nonterminals & terminals.
+  grammarString = grammarString.trim();
   var errorBox = document.getElementById("grammar_errors");
   var codeInput = document.getElementById("code");
 
   errorBox.innerHTML = "";
 
   try {
-    productions = getProductions(grammarString);
-    validSymbols = getValidSymbols(productions);
+    var productions = getProductions(grammarString);
+    var validSymbols = getValidSymbols(productions);
+    var terminals = validSymbols.terminals;
+    var nonterminals = validSymbols.nonterminals;
+
+    // Grammar built. Allow interpretation.
     codeInput.disabled = false;
+    console.log(terminals);
+    console.log(nonterminals);
   } catch (err) {
+    console.log(err);
     err = 'Error: ' + err;
     errorBox.innerHTML = err;
     codeInput.disabled = true;
